096a9eea70e26e2ec47c3a73233102d4b74d87fd
lorenaostroski <168372083+lorenaostroski@users.noreply.github.com>
2024-08-08 19:21:26 +0200

    Uvjetno grananje switch

---
+++ CSHARP/Ucenje/UcenjeCS/E04UvjetnoGrananjeSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E04UvjetnoGrananjeSwitch
+    {
+
+        public static void Izvedi() 
+        {
+            //ako je i==0 ispiši DOBAR,1 LOŠ,2 ZAO
+
+            int i= 0;
+
+            switch (i)//radi s int,string i char tipovima podataka
+            {
+                case 0:
+                    Console.WriteLine("DOBAR");
+                    break;
+                case 1:
+                    Console.WriteLine("Loš");
+                    break;
+                case 2:
+                    Console.WriteLine("ZAO");
+                    break;
+                default://TO JE ELSE U IF-U
+                    Console.WriteLine("Nije definirano");
+                    break;
+
+
+
+
+
+            }
+        
+        
+        }
+    }
+}
---
+++ CSHARP/Ucenje/UcenjeCS/E04Z1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E04Z1
+    {
+
+        public static void Izvedi() 
+        {
+            //ZADATAK:
+            //Korisnik unosi brojčani iznos ocjene.Program ispisuje 
+            //tekstualno ocjenu
+
+            int ocjena;
+            Console.WriteLine("Unesite ocjenu u brojčanom obliku: ");
+            ocjena=int.Parse(Console.ReadLine());
+
+            switch (ocjena)
+            {
+                case 0:
+                    Console.WriteLine("DOBAR");
+                    break;
+                case 1:
+                    Console.WriteLine("ODLIČAN");
+                    break;
+                default:
+                    Console.WriteLine("NIJE DEFINIRANO");
+                    break;
+
+
+            }
+
+
+
+
+
+
+           
+
+
+
+
+        }
+    }
+}
