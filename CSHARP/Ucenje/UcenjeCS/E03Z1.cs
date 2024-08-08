34ad733bb1d5f32a88ba602a256ba92931e475be
lorenaostroski <168372083+lorenaostroski@users.noreply.github.com>
2024-08-08 19:20:20 +0200

    Uvjetno grananje if

---
+++ CSHARP/Ucenje/UcenjeCS/E03Z1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E03Z1
+    {
+
+        public static void Izvedi() 
+        {
+            //zadatak:
+            //Program od korisnika traži unos broj godina koje ima korisnik
+            //Program ispisuje dali je korisnik punoljetne osoba ili ne
+
+            Console.WriteLine("Unesite broj godina koje imate: ");
+            var godine= int.Parse(Console.ReadLine());
+
+
+            if (godine >= 18)
+            {
+                Console.WriteLine("Punoljetni ste.");
+            
+            }
+            else 
+            {
+                Console.WriteLine("Niste punoljestni.");
+            }
+
+            //ili još jednostavnije
+
+            Console.WriteLine("Unesi broj godina: ");
+            var g= int.Parse(Console.ReadLine());
+            Console.WriteLine(g>=18? "Punoljetna":"Maloljetna");
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ CSHARP/Ucenje/UcenjeCS/E03Z2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+{
+    internal class E03Z2
+    {
+        public static void Izvedi()
+        {
+            //Zadatak:
+            //Korisnik unosi dva decimalna broja
+            //Program ispisuje Veći ili Jednaki su ako su uneseni brojevi jednaki
+
+            float[] b = new float[2];
+            Console.Write("Unesi prvi broj: ");
+            b[0] = float.Parse(Console.ReadLine());
+            Console.Write("Unesi drugi broj: ");
+            b[1] = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(
+                b[1] == b[0] ? "Jednaki su" :
+                b[0] > b[1] ? b[0] : b[1]);
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
