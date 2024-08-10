9ea254dbcfbfa1566fa9b77ade040998e61f74f0
lorenaostroski <168372083+lorenaostroski@users.noreply.github.com>
2024-08-10 12:40:33 +0200

    Zadaci

---
+++ CSHARP/Ucenje/UcenjeCS/E10Z1.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace UcenjeCS
+{
+    internal class E10Z1
+    {
+        public static void Izvedi() 
+        {
+            //Program od korisnika unosi 10 brojeva
+            //Program ispisuje prvo zbroj svih unesenih brojeva
+            //i nakon toga ispisuje unesene brojeve jedno ispod drugog
+
+            //primjer: nakon svih unosa ispisuje se
+            //10
+            //1
+            //3
+            //0
+            //0
+            //1
+            //1
+            //1
+            //1
+            //1
+
+            int ub = 10;//ovov može i  korisnik unijeti
+            int[]brojevi=new int[ub];
+
+            for (int i = 0; i < ub; i++) 
+            {
+                Console.WriteLine("Unesi{0}. broj:", i+1);
+                brojevi[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            int zbroj = 0;
+            foreach (var b in brojevi) 
+            {
+                zbroj += b;
+            }
+            Console.WriteLine(zbroj);
+            int index = 0;
+            while (index < ub) 
+            {
+                Console.WriteLine(brojevi[index++]);
+            }
+        }
+    }
+}
---
+++ CSHARP/Ucenje/UcenjeCS/E10Z2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS
+    //Za unesenu riječ provjerite je li palindrom
+    //Palindrom je riječ koja se jednako čita s obje strane
+    //anavolimilovana,02022020,evolove,evoove,ananabraparbanana
+{
+    internal class E10Z2
+    {
+        public static void Izvedi() 
+        {
+            Console.WriteLine("Unesi riječ:");
+            string rijec=Console.ReadLine();
+            bool palindrom = true;
+            for (int i = 0; i<rijec.Length/2; i++) 
+            {
+                if (rijec[i] != rijec[rijec.Length - 1 - i]) 
+                {
+                    palindrom = false;
+                    break;
+                }
+            }
+            Console.WriteLine("Riječ {0} {1} palindrom", rijec,palindrom? "JE":"NIJE");
+        }
+    }
+}
