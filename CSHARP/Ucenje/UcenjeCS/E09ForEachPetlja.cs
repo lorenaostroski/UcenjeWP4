77615a0895038643ccd56dad2a6eda46befb38c6
lorenaostroski <168372083+lorenaostroski@users.noreply.github.com>
2024-08-10 12:35:54 +0200

    ForEachPetlja

---
+++ CSHARP/Ucenje/UcenjeCS/E09ForEachPetlja.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace UcenjeCS
+{
+    internal class E09ForEachPetlja
+    {
+        public static void Izvedi()
+        {
+            // foreach je skraćeni for
+            // Ispiši svaki znak iz riječ jedno ispod drugog
+            string rijec = "Osijek";
+
+            for (int i = 0; i < rijec.Length; i++)
+            {
+                Console.WriteLine(rijec[i]);
+            }
+
+            foreach (var slovo in rijec)
+            { // foreach radi s nizovima, stringovima (koji su niz znakova), listama, rječnicima, ...
+                Console.WriteLine(slovo);
+            }
+            /* ne ide
+            int b = 25252;
+            foreach(var b1 in b)
+            {
+
+            }
+            */
+        }
+
+
+
+
+
+
+    }
+}
