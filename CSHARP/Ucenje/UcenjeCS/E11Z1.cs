f3e1fbd7d4d8f530f1403318b65efaf3ff64439a
lorenaostroski <168372083+lorenaostroski@users.noreply.github.com>
2024-08-18 15:59:12 +0200

    E11Z1

---
+++ CSHARP/Ucenje/UcenjeCS/E11Z1.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace UcenjeCS
+{
+    internal class E11Z1
+    {
+
+        // za dva učitana broja koja unosi korisnik ispiši zbroj
+        public static void Izvedi()
+        {
+            int b1 = Pomocno.UcitajCijeliBroj("Unesi 1. broj");
+            int b2 = Pomocno.UcitajCijeliBroj();
+            Console.WriteLine(b1 + b2);
+
+
+
+
+
+    }
+        }
+}   
