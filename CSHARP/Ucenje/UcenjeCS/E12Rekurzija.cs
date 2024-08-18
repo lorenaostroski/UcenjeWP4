e5e8f3948659835b3e16086b537f11f20563d008
lorenaostroski <168372083+lorenaostroski@users.noreply.github.com>
2024-08-18 16:00:08 +0200

    E12Rekurzija

---
+++ CSHARP/Ucenje/UcenjeCS/E12Rekurzija.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace UcenjeCS
+{
+    internal class Rekurzija
+    {
+
+
+        // Rekurzija je kada metoda poziva samu sebe - nedovoljna definicija
+
+        public static void Izvedi()
+        {
+            do
+            {
+                // LosaMetoda();
+                Console.WriteLine(Zbroj(Pomocno.UcitajCijeliBroj("Unesi broj za sumu svih brojeva do tog broja")));
+            } while (Pomocno.UcitajCijeliBroj("-1 za prekid") != -1);
+
+        }
+
+
+        private static int Zbroj(int broj)
+        {
+            // uvijek prvo napraviti uvije prekida kekurzije
+            if (broj == 0)
+            {
+                return broj;
+            }
+            return broj + Zbroj(broj - 1);
+        }
+
+
+
+        static void LosaMetoda()
+        {
+            LosaMetoda(); // stackoverflow exception
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
