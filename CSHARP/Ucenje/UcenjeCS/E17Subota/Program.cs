f4e278a112ca2ccb3633ee2b93c076d0d15a47aa
lorenaostroski <168372083+lorenaostroski@users.noreply.github.com>
2024-08-24 23:58:09 +0200

    E17Subota

---
+++ CSHARP/Ucenje/UcenjeCS/E17Subota/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.E17Subota
+{
+    internal class Program
+    {
+        public Program() 
+        {
+            var ime1 = "ana";
+            var ime2 = "ivo";
+
+            bool ispravno = true;
+
+            foreach (var z in ime1)
+            {
+                if (!char.IsLetter(z))
+                {
+                    ispravno = false;
+                    break;
+                }
+            }
+
+            if (!ispravno)
+            {
+                Console.WriteLine("nije dobro");
+                return;
+            }
+
+
+            for (int i = 0; i < 100; i++)
+            {
+               /* Console.WriteLine(Faker.Name.FullName(NameFormats.StandardWithMiddleWithPrefix));*/
+
+            }
+
+            string br = "6";
+            int b;
+            if (!int.TryParse(br, out b))
+            {
+                Console.WriteLine("Ne valja");
+            }
+            Console.WriteLine(b);
+
+            int kratko;
+            int.TryParse("7", out kratko);
+
+            Console.WriteLine(kratko);
+
+        }
+
+        public object Faker { get; }
+        public object NameFormats { get; }
+    }
+}
---
+++ CSHARP/Ucenje/UcenjeCS/E17Subota/Raspored.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UcenjeCS.E17Subota
+{
+    internal class Raspored
+    {
+        private static object return1;
+
+        public Raspored() 
+        {
+            // u ljetim mjesecim asvakih mjesec dana a u zimskim svaka dva tjeda
+
+            DateTime datumOd = DateTime.Parse("2024-07-08");
+            DateTime datumDo = datumOd;
+            DateTime tjedan = datumOd;
+            int broj = 0;
+
+            while (datumOd.Year < 2028)
+            {
+                if (tjedan.Month >= 4 && tjedan.Month <= 10)
+                {
+                    datumDo = datumOd.AddDays(27);
+                }
+                else
+                {
+                    datumDo = datumOd.AddDays(6);
+                }
+                tjedan = datumOd.AddDays(6);
+
+                Console.WriteLine("{0} - {1}, Stan {2}",
+                    datumOd.ToString("dd.MM.yyyy."),
+                    tjedan.ToString("dd.MM.yyyy."),++broj%3+1 );
+                datumOd = datumDo.AddDays(1);
+            }    
+        
+            
+        }
+    }
+}
